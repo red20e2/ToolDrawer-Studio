e463f70915445dfa352585635c5425a390f6ea92
red20e2 <red20e2@gmail.com>
2026-08-11 12:30:32 -0600

    chore: bootstrap ToolDrawer Studio domain

---
+++ src/tooldrawer_studio/domain/__init__.py
@@ -0,0 +1,5 @@
+"""Domain contracts for ToolDrawer Studio."""
+
+from .models import CalibrationRecord, CaptureAsset, Point2D, Project, ToolObject
+
+__all__ = ["CalibrationRecord", "CaptureAsset", "Point2D", "Project", "ToolObject"]
