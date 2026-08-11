995d6c6428ce5a94a59bba6eb5d2037a10b6b830
red20e2 <red20e2@gmail.com>
2026-08-11 15:56:48 -0600

    feat: expose contour geometry

---
+++ src/tooldrawer_studio/geometry/__init__.py
@@ -0,0 +1,13 @@
+from .contour import (
+    polygon_area_mm2,
+    replace_tool_contour,
+    reset_tool_contour,
+    validate_contour,
+)
+
+__all__ = [
+    "polygon_area_mm2",
+    "replace_tool_contour",
+    "reset_tool_contour",
+    "validate_contour",
+]
