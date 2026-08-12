10525c1e839587e7284af5e1a478efc50f0e2961
red20e2 <red20e2@gmail.com>
2026-08-12 12:00:37 -0600

    test: remove obsolete runtime tool depth

--- tests/unit/test_pocket.py
+++ tests/unit/test_pocket.py
@@ -6,7 +6,14 @@ from tooldrawer_studio.geometry.pocket import PocketSpec, build_pocket_insert
 
 def _tool() -> ToolObject:
     contour = [Point2D(10, 10), Point2D(30, 10), Point2D(30, 20), Point2D(10, 20)]
-    return ToolObject(id="tool-1", name="Block", source_capture_id="capture-1", base_contour_mm=list(contour), contour_mm=list(contour), clearance_mm=0.5, depth_mm=4.0)
+    return ToolObject(
+        id="tool-1",
+        name="Block",
+        source_capture_id="capture-1",
+        base_contour_mm=list(contour),
+        contour_mm=list(contour),
+        clearance_mm=0.5,
+    )
 
 
 def test_build_pocket_insert_has_expected_outer_dimensions():
