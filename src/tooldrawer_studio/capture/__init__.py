5731845c279da0e7eade9cb4aa5ce6923fc966f5
red20e2 <red20e2@gmail.com>
2026-08-11 18:31:56 -0600

    fix: expose stored image decoder

--- src/tooldrawer_studio/capture/__init__.py
+++ src/tooldrawer_studio/capture/__init__.py
@@ -1,3 +1,3 @@
-from .image_loader import LoadedImage, load_image
+from .image_loader import LoadedImage, load_image, load_image_bytes
 
-__all__ = ["LoadedImage", "load_image"]
+__all__ = ["LoadedImage", "load_image", "load_image_bytes"]
