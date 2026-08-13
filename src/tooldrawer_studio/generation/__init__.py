f9f9b994a41ddc4fa939b93b5e88e2bb5819c607
red20e2 <red20e2@gmail.com>
2026-08-12 19:43:56 -0600

    feat: expose generation contracts

---
+++ src/tooldrawer_studio/generation/__init__.py
@@ -0,0 +1,13 @@
+from tooldrawer_studio.generation.models import (
+    GenerationSettings,
+    GenerationState,
+    HeightMode,
+    ScoopMode,
+)
+
+__all__ = [
+    "GenerationSettings",
+    "GenerationState",
+    "HeightMode",
+    "ScoopMode",
+]
